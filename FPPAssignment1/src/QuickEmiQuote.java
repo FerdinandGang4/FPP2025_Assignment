d3b43a595e0e88c734ea1b66f7d71c51bad5e5fc
Gang Ferdinand Dinga <ferdinand.gang4@gmail.com>
2025-09-01 22:10:16 -0500

    Update assigment

--- FPPAssignment1/src/QuickEmiQuote.java
+++ FPPAssignment1/src/QuickEmiQuote.java
@@ -6,59 +6,38 @@ public class QuickEmiQuote {
     private static int principal_p;
     private static int tenure_n;
     private static double anualInterestRate_APR;
-    private static double totalPayment;
-    private static double totalInterest;
+    private static int totalPayment;
+    private static int totalInterest;
     private  static double monthlyRate_r;
+    private static double emi;
     private static Random random = new Random();
 
     public static void main(String[] args) {
         randmomizeInputs();
-        System.out.println("Principal= "+ principal_p +
-                           "  tenure= " +tenure_n +
-                           "  anual interest= " + anualInterestRate_APR);
+        computeEmi();
+        tryAsampleQuote();
     }
-
 
     static void randmomizeInputs()
     {
       principal_p = random.nextInt(5000,25001);
-      tenure_n = random.nextInt(6,36);
-      anualInterestRate_APR = RandomGenerator.getDefault().nextDouble(3.5,11.5);
+      tenure_n = random.nextInt(6,37);
+      anualInterestRate_APR = Math.round( RandomGenerator.getDefault().nextDouble(3.5,11.5) *10)/10.0;
     }
 
-    /*
-    * Compute EMI (Equated Monthly Installment – Use Math API)
-     Monthly rate r = APR / 12 / 100
-     EMI formula (use Math.pow):
-    * */
-    static  double calculateEmi()
+    static void computeEmi()
     {
-        monthlyRate_r = anualInterestRate_APR/(12/100);
-
-        double emi = principal_p*monthlyRate_r*
-                    Math.pow(1+monthlyRate_r,tenure_n)/
-                    Math.pow(1+monthlyRate_r,tenure_n) -1;
-
-        return emi;
+        monthlyRate_r = anualInterestRate_APR / 12 / 100;
+        emi = (principal_p * monthlyRate_r) * Math.pow(1 + monthlyRate_r,tenure_n)/ Math.pow(1 + monthlyRate_r, tenure_n) -1;
+        totalPayment = Math.round((int)emi) * tenure_n;
+        totalInterest = totalPayment - principal_p;
     }
 
-    static double getTotalPayment()
+    static void tryAsampleQuote()
     {
-        return calculateEmi() * tenure_n;
-    }
-
-    static double getTotalInterest()
-    {
-        return getTotalPayment() - principal_p;
-    }
-    /*
-     Show multiple integer views of the EMI
-    * */
-    static void showMultipleIntegerViews()
-    {
-        System.out.printf("Integer value of EMI %d.2  is ", calculateEmi());
-        //Rounded EMI to the nearest int using Math.round (e.g., $313)
-        System.out.printf("Rounded value of EMI to the nearest is ", Math.round(calculateEmi()));
+        System.out.println("Monthly rate   :" + monthlyRate_r);
+        System.out.println("Total payment  :" + totalPayment);
+        System.out.println("Total Interest :" + totalInterest);
     }
 }
 
